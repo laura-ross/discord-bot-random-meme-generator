ec04c2eef79ea6874cb5e6de86e3bee68069be2f
LauraE2020 <smalltowngirl591@gmail.com>
2020-10-10 17:20:32 -0400

    Added project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Random Meme Generator Discord Bot
+
+# Tech used
+  This discord bot was created using discord.js.
+
+# Usage
+
+- Type the command __"!rp"__ and it will generate a random __puppy image__
+
+- Type in the command __"!rm"__ and it will generate a __random meme__ from reddit
+
+# Contributing
+Pull requests are welcome. Node.js must be installed in order to download this repository.
---
+++ index.js
@@ -0,0 +1,49 @@
+
+const { Client, MessageAttachment, MessageEmbed } = require("discord.js");
+const client = new Client();
+const { token, prefix } = require("./config.js");
+const randomPuppy = require('random-puppy');
+
+// The randomPuppy npm package generates random memes when you pass in a subreddit as an argument. If there is no argument, it generates a photo of a random puppy.
+
+const subreddits = [
+  "meme",
+  "memes",
+  "MemeEconomy",
+  "Memes_Of_The_Dank",
+  "MemesIRL",
+  "dankmemes",
+  "AdviceAnimals",
+  "ComedyCemetery",
+  "terriblefacebookmemes",
+  "funny"
+];
+
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`)
+});
+
+// random puppy command
+client.on("message", message => {
+  if(message.content === `${prefix}rp`) {
+    randomPuppy().then(url => {
+      const randomPuppy = new MessageAttachment(url);
+      message.channel.send(randomPuppy);
+    })
+  }
+});
+
+// random meme command
+client.on("message", message => {
+  if(message.content === `${prefix}rm`) {
+    const i = Math.abs(Math.floor(Math.random() * subreddits.length - 1));
+    randomPuppy(subreddits[i]).then(url => {
+      const randomMeme = new MessageAttachment(url);
+      message.channel.send(randomMeme);
+      console.log(i)
+    })
+  }
+})
+
+client.login(token);
